46b9267f7a7d7c91a460a210b0dfd2427f1e274c
unknown <jonaseal17@gmail.com>
2022-04-25 20:05:10 -0300

    Exercicio 1002

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exer1002
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const double n = 3.14159;
+            double raio = double.Parse(Console.ReadLine());
+            double area = n * (raio * raio);
+
+            Console.WriteLine("A = " + area.ToString("F4"));
+        }
+    }
+}
